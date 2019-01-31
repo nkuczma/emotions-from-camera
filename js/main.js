4f8f49b64333a7c26b291d0348dfd6390509a902
nkuczma <nkuczma@wp.pl>
2019-01-31 01:32:12 +0100

    sending photos to api, styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var videoElement = document.querySelector('video');
-var videoSelect = document.querySelector('select#videoSource');
+var videoSelect = document.querySelector('#videoSource');
+var takePhotoTimer;
 
 navigator.mediaDevices.enumerateDevices()
   .then(gotDevices).then(getStream).catch(handleError);
@@ -31,9 +32,7 @@ function getStream() {
   }
 
   var constraints = {
-    video: {
-      deviceId: {exact: videoSelect.value}
-    }
+    video: { deviceId: {exact: videoSelect.value} }
   };
 
   navigator.mediaDevices.getUserMedia(constraints).
@@ -41,7 +40,7 @@ function getStream() {
 }
 
 function gotStream(stream) {
-  window.stream = stream; // make stream available to console
+  window.stream = stream; 
   videoElement.srcObject = stream;
 }
 
@@ -49,16 +48,71 @@ function handleError(error) {
   console.log('Error: ', error);
 }
 
-//////////// screen
-const screenshotButton = document.querySelector('#screenshot-button');
-const img = document.querySelector('.select img');
+//// screen
+const startButton = document.querySelector('#start-button');
+const stopButton = document.querySelector('#stop-button');
 const canvas = document.createElement('canvas');
 
-screenshotButton.onclick = videoElement.onclick = function() {
+startButton.onclick = videoElement.onclick = function() {
+  takePhotoTimer = setInterval(takePhoto, 4500);
+};
+
+stopButton.onclick = videoElement.onclick = function() {
+  clearInterval(takePhotoTimer);
+};
+
+function processResult(res) {
+  if(res.length>0) {
+    let emotions = res[0].faceAttributes.emotion;
+    let maxEmotion = Object.keys(emotions).reduce((a, b) => emotions[a] > emotions[b] ? a : b);
+    return maxEmotion[0].toUpperCase() + maxEmotion.slice(1);
+  }
+  else {
+    return "Unknown";
+  }
+}
+
+function takePhoto(){
   canvas.width = videoElement.videoWidth;
   canvas.height = videoElement.videoHeight;
   canvas.getContext('2d').drawImage(videoElement, 0, 0);
-  // Other browsers will fall back to image/png
-  var imgg = canvas.toDataURL('image/webp');
-  console.log(imgg);
+  var img = canvas.toDataURL('image/jpeg');
+  processImage(img)
+}
+
+//// emotion recognition
+function processImage(img) {
+  var subscriptionKey = document.getElementById("inputKey").value;
+  var uriBase = "https://northeurope.api.cognitive.microsoft.com/face/v1.0/detect";
+
+  var params = {
+      "returnFaceId": "true",
+      "returnFaceLandmarks": "false",
+      "returnFaceAttributes":
+          "emotion"
+  };
+
+  fetch(img)
+    .then(res => res.blob())
+    .then(blobData => {
+      $.post({
+          url: uriBase + "?" + $.param(params),
+          contentType: "application/octet-stream",
+          headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': '85e993b533e64898a9cf7fc6be2b968d'
+          },
+          crossDomain: true,
+          processData: false,
+          data: blobData
+        })
+        .done(function(data) {
+          console.log(data);
+          $("#results").text(processResult(data));
+
+        })
+        .fail(function(err) {
+          $("#results").text(JSON.stringify(err));
+        })
+    });
 };
